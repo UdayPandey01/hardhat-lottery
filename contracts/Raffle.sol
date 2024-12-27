bf345d06a75104e600e425a9ab5a94cdb759d9ae
UdayPandey01 <pandeyuday113@gmail.com>
2024-12-27 11:25:53 +0530

    raffleUpdated

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -6,6 +6,7 @@ import "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.s
 import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
 error Raffle__notEnoughEthEntered();
+error Raffle__transferfailed();
 
 contract Raffle is VRFConsumerBaseV2 {
     /*State variable*/
@@ -18,11 +19,21 @@ contract Raffle is VRFConsumerBaseV2 {
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
 
+    /*Lottery Variables */
+    address private s_recentWinner;
+
     /*Events*/
     event RaffleEnter(address indexed player);
     event RequestRaffleWinner(uint256 indexed requestId);
+    event winnerPicked(address indexed winner);
 
-    constructor(address vrfCoordinatorV2, uint256 entranceFees, bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit) VRFConsumerBaseV2(vrfCoordinatorV2){
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFees,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFees = entranceFees;
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_gasLane = gasLane;
@@ -31,7 +42,9 @@ contract Raffle is VRFConsumerBaseV2 {
     }
 
     function enterRaffle() public payable {
-        if(msg.value < i_entranceFees){revert Raffle__notEnoughEthEntered();}
+        if (msg.value < i_entranceFees) {
+            revert Raffle__notEnoughEthEntered();
+        }
         s_players.push(payable(msg.sender));
         emit RaffleEnter(msg.sender);
     }
@@ -48,15 +61,29 @@ contract Raffle is VRFConsumerBaseV2 {
         emit RequestRaffleWinner(requestId);
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override{
-
+    function fulfillRandomWords(
+        uint256 /*requestId,*/,
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__transferfailed();
+        }
+        emit winnerPicked(recentWinner);
     }
 
-    function getEntranceFees() public view returns (uint256){
+    function getEntranceFees() public view returns (uint256) {
         return i_entranceFees;
     }
 
-    function getPlayer(uint256 index) public view returns(address){
+    function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
     }
 }
