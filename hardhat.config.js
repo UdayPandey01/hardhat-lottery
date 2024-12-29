bffaf80e44218da6818e6d6d80b52f3c94bc413b
UdayPandey01 <pandeyuday113@gmail.com>
2024-12-29 19:23:59 +0530

    raffle deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,35 @@ require("hardhat-contract-sizer")
 require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+
 module.exports = {
+  defaultNetwork : "hardhat",
+  networks : {
+    default : {
+      hardhat : {
+        chainId : 31337,
+        blockConfirmations : 1
+      },
+      sepolia : {
+        chainId : 11155111,
+        blockConfirmations : 6,
+        url : SEPOLIA_RPC_URL,
+        accounts : [PRIVATE_KEY]
+      }
+    }
+  },
   solidity: "0.8.28",
+  namedAccounts : {
+    deployer : {
+      default : 0,
+    },
+    player : {
+      default : 1,
+    }
+  }
 };
