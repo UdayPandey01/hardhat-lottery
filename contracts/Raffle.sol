a08dc804d172a339c9090927f39ba294ccff6dc8
UdayPandey01 <pandeyuday113@gmail.com>
2024-12-29 15:40:33 +0530

    changes KeeperCompatible to AutomationCompatible

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -4,20 +4,24 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/automation/AutomationCompatible.sol";
 
 error Raffle__notEnoughEthEntered();
 error Raffle__transferfailed();
 error Raffle__NotOpen();
-error Raffle__UpkeepNotNeeded(uint256 currentBalance, uint256 numPlayers, uint256 raffleState);
+error Raffle__UpkeepNotNeeded(
+    uint256 currentBalance,
+    uint256 numPlayers,
+    uint256 raffleState
+);
 
-contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
+contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
     /*Type State*/
     enum RaffleState {
         OPEN,
         CALCULATING
     }
-    
+
     /*State variable*/
     uint256 private immutable i_entranceFees;
     address payable[] private s_players;
@@ -43,7 +47,7 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
         address vrfCoordinatorV2,
         uint256 entranceFees,
         bytes32 gasLane,
-        uint64 subscriptionId, 
+        uint64 subscriptionId,
         uint32 callbackGasLimit,
         uint256 interval
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
@@ -62,26 +66,38 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
             revert Raffle__notEnoughEthEntered();
         }
 
-        if(s_raffleState != RaffleState.OPEN){
+        if (s_raffleState != RaffleState.OPEN) {
             revert Raffle__NotOpen();
         }
 
         s_players.push(payable(msg.sender));
-        emit RaffleEnter(msg.sender); 
+        emit RaffleEnter(msg.sender);
     }
 
-    function checkUpkeep(bytes memory /*checkData*/) public override returns(bool upkeepNeeded , bytes memory /*performData*/) {
-        bool isOpen = (RaffleState.OPEN == s_raffleState)
+    function checkUpkeep(
+        bytes memory /*checkData*/
+    )
+        public
+        view
+        override
+        returns (bool upkeepNeeded, bytes memory /*performData*/)
+    {
+        bool isOpen = (RaffleState.OPEN == s_raffleState);
         bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
         bool hasPlayers = (s_players.length > 0);
         bool hasBalance = address(this).balance > 0;
         upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
+        return (upkeepNeeded, "");
     }
 
     function performUpkeep(bytes calldata /*performData*/) external {
         (bool upkeepNeeded, ) = checkUpkeep("");
-        if(!upkeepNeeded){
-            revert Raffle__UpkeepNotNeeded(address(this).balance, s_players.length, uint256(s_raffleState));
+        if (!upkeepNeeded) {
+            revert Raffle__UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_raffleState)
+            );
         }
 
         s_raffleState = RaffleState.CALCULATING;
@@ -113,7 +129,7 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
         if (!success) {
             revert Raffle__transferfailed();
         }
-        
+
         emit winnerPicked(recentWinner);
     }
 
@@ -129,22 +145,23 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
         return s_recentWinner;
     }
 
-    function getRaffleState() public view returns(RaffleState){
+    function getRaffleState() public view returns (RaffleState) {
         return s_raffleState;
     }
 
-    function getNumWords() public pure returns(uint256){
+    function getNumWords() public pure returns (uint256) {
         return NUM_WORDS;
     }
-    function getNumberOfPlayers() public view returns(uint256){
+
+    function getNumberOfPlayers() public view returns (uint256) {
         return s_players.length;
     }
 
-    function getLatestTimeStamp() public view returns(uint256){
+    function getLatestTimeStamp() public view returns (uint256) {
         return s_lastTimeStamp;
     }
 
-    function getRequestConfirmation() public pure returns(uint256){
+    function getRequestConfirmation() public pure returns (uint256) {
         return REQUEST_CONFIRMATIONS;
     }
 }
