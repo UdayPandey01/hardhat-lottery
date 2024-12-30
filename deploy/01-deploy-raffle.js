bc95c9692842c72f2c4a0fbfeb78d792ac9225d5
UdayPandey01 <pandeyuday113@gmail.com>
2024-12-30 23:56:33 +0530

    test

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -3,7 +3,6 @@ const { network } = require("hardhat")
 module.exports = async function({getNamedAccounts, deployments}){
     const {deploy, log} = deployments
     const {deployer} = await getNamedAccounts()
-
     const raffle = await deploy("Raffle", {
         from : deployer,
         args : [],
